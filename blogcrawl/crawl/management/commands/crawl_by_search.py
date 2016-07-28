4e60b58de3f8305557bd44674128b4079ef3db24
Elkiscoming <Elkiscoming@gmail.com>
2016-07-28 12:42:54 +0430

    now search in page 9 too and have better log.

--- blogcrawl/crawl/management/commands/crawl_by_search.py
+++ blogcrawl/crawl/management/commands/crawl_by_search.py
@@ -5,32 +5,6 @@ import requests
 import re
 import time
 
-def crawl_word_start(word, start=0):
-    while True:
-        try:
-            r = requests.get('http://blogs.salam.ir/blog.ir?q=' + word + '&start=' + str(start))
-        except requests.ConnectionError:
-            continue
-        break
-    soup = BeautifulSoup(r.text, 'html.parser')
-    for a in soup.find_all('a'):
-        try:
-            link = a['href']
-            m = re.match(r'http://(?P<name>\w+)\.blog.ir/', link)
-            if m and not Blog.objects.filter(name=m.group('name')):
-                print("new blog found: " + m.group('name'))
-                blog = Blog()
-                blog.name = m.group('name')
-                blog.crawl_status = 'N'
-                blog.save()
-        except KeyError:
-            pass
-
-def crawl_word(word):
-    for i in range(0,9):
-        print('page: ' + str(i))
-        crawl_word_start(word, i * 10)
-
 class Command(BaseCommand):
     help = 'crawl blogs by search word in blogs'
 
@@ -39,4 +13,30 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for word in options['word']:
-            crawl_word(word)
+            self.crawl_word(word)
+
+    def crawl_word_start(self, word, start=0):
+        while True:
+            try:
+                r = requests.get('http://blogs.salam.ir/blog.ir?q=' + word + '&start=' + str(start))
+            except requests.ConnectionError:
+                continue
+            break
+        soup = BeautifulSoup(r.text, 'html.parser')
+        for a in soup.find_all('a'):
+            try:
+                link = a['href']
+                m = re.match(r'http://(?P<name>\w+)\.blog.ir/', link)
+                if m and not Blog.objects.filter(name=m.group('name')):
+                    print("new blog found: " + m.group('name'))
+                    blog = Blog()
+                    blog.name = m.group('name')
+                    blog.crawl_status = 'N'
+                    blog.save()
+            except KeyError:
+                pass
+
+    def crawl_word(self, word):
+        for i in range(0, 10):
+            self.stdout.write('search for: ' + word + ', page: ' + str(i))
+            self.crawl_word_start(word, i * 10)
