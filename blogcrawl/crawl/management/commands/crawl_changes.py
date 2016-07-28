9f569db8aad43ce11987bcc0292595fd8d0a97ed
Elkiscoming <Elkiscoming@gmail.com>
2016-07-28 11:14:16 +0430

    infinitly crawl changes with wait!

--- blogcrawl/crawl/management/commands/crawl_changes.py
+++ blogcrawl/crawl/management/commands/crawl_changes.py
@@ -3,29 +3,45 @@ from crawl.models import Blog
 from bs4 import BeautifulSoup
 import requests
 import re
+import time
+
+def crawl_blog_changes():
+    while True:
+        try:
+            r = requests.get('http://blog.ir/changes')
+        except requests.ConnectionError:
+            continue
+        break
+    soup = BeautifulSoup(r.text, 'html.parser')
+    post_divs = soup.find_all("div", class_='post')
+    for post in post_divs:
+        for a in post.find_all('a'):
+            link = a['href']
+            m = re.match(r'http://(?P<name>\w+)\.blog.ir/', link)
+            if m and not Blog.objects.filter(name=m.group('name')):
+                print("new blog found: " + m.group('name'))
+                blog = Blog()
+                blog.name = m.group('name')
+                blog.crawl_status = 'N'
+                blog.save()
 
 class Command(BaseCommand):
     help = 'crawl recently changed blogs'
 
     def add_arguments(self, parser):
-        pass
+        parser.add_argument(
+            '--wait',
+            dest='wait',
+            default=0,
+            help='how many seconds to wait',
+        )
 
     def handle(self, *args, **options):
-        while True:
+        crawl_blog_changes()
+        if options['wait'] != 0:
             try:
-                r = requests.get('http://blog.ir/changes')
-            except requests.ConnectionError:
-                continue
-            break
-        soup = BeautifulSoup(r.text, 'html.parser')
-        post_divs = soup.find_all("div", class_='post')
-        for post in post_divs:
-            for a in post.find_all('a'):
-                link = a['href']
-                m = re.match(r'http://(?P<name>\w+)\.blog.ir/', link)
-                if m and not Blog.objects.filter(name=m.group('name')):
-                    print("new blog found: " + m.group('name'))
-                    blog = Blog()
-                    blog.name = m.group('name')
-                    blog.crawl_status = 'N'
-                    blog.save()
+                while True:
+                    time.sleep(float(options['wait']))
+                    crawl_blog_changes()
+            except:
+                self.stderr.write(self.style.ERROR('terminated'))
