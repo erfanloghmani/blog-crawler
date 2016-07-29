9043ed4ffab0f003a25f77f5bdb7b0678e795dd0
Elkiscoming <Elkiscoming@gmail.com>
2016-07-29 11:48:15 +0430

    index page show number of blogs and links.

--- blogcrawl/crawl/views.py
+++ blogcrawl/crawl/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Blog, Link
 # Create your views here.
 
+def index(request):
+    blog_count = len(Blog.objects.all())
+    link_count = len(Link.objects.all())
+    return HttpResponse('#blog: ' + str(blog_count) + ' #link: ' + str(link_count))
