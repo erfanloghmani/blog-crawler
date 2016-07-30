efd681226060e70fc9649393c1587250fe427103
Elkiscoming <Elkiscoming@gmail.com>
2016-07-30 14:12:54 +0430

    dest degree output.

--- blogcrawl/crawl/urls.py
+++ blogcrawl/crawl/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'/count$', views.link_count),
 ]
--- blogcrawl/crawl/views.py
+++ blogcrawl/crawl/views.py
@@ -7,3 +7,10 @@ def index(request):
     blog_count = len(Blog.objects.all())
     link_count = len(Link.objects.all())
     return HttpResponse('#blog: ' + str(blog_count) + ' #link: ' + str(link_count))
+
+def link_count(request):
+    counts = []
+    for blog in Blog.objects.all():
+        counts.append((len(Link.objects.filter(dest=blog)), blog.name))
+    counts.sort()
+    return HttpResponse(str(counts))
