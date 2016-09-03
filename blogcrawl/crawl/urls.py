08468a35ecafbdf253cf275c9b80767fbf16ce8e
Elkiscoming <Elkiscoming@gmail.com>
2016-09-03 11:56:57 +0430

    add coeffition.

--- blogcrawl/crawl/urls.py
+++ blogcrawl/crawl/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     url(r'^$', views.index),
     url(r'^blog/(?P<blog>\w+)$', views.blog),
     url(r'^in_degrees/(?P<page>[0-9]+)$', views.in_degrees),
+    url(r'^coef/(?P<page>[0-9]+)$', views.coef),
     url(r'^out_degrees/(?P<page>[0-9]+)$', views.out_degrees),
     url(r'^api/', include(router.urls)),
     url(r'count$', views.link_count),
